fdb1c8275d3fe570bbed1c3d21055c0049dccde6
DesarrolloColectivo <zasalastobon@gmail.com>
2022-01-12 21:20:12 -0500

    add auth0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { LoginButton } from "./components/LoginButton";
+import { Profile } from "./components/Profile";
+import { LogoutButton } from "./components/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <picture className="header-picture">
+          <div className="App-header-login">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Imagotipo_de_la_Universidad_de_la_Amazonia.svg/1024px-Imagotipo_de_la_Universidad_de_la_Amazonia.svg.png"
+              className="App-logo"
+              alt="logo"
+            />
+          </div>
+        </picture>
+
+        <p App-header-login>
+          {isAuthenticated ? <LogoutButton /> : <LoginButton />}{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <Profile />
+      </main>
+      <footer>Desarrollo Colectivo 2020</footer>
     </div>
   );
 }
